a9069017243aefa18f4edf69697ee0915fb607fd
Evegen <gormoshka123@gmail.com>
2023-09-21 18:23:52 +0300

    with the privous one

--- frontend/src/Pages/EditEvent.js
+++ frontend/src/Pages/EditEvent.js
@@ -1,5 +1,11 @@
+import EventForm from "../components/EventForm";
+import { useRouteLoaderData, json, redirect } from "react-router-dom";
+
+//"UseRoaterLoaderData" hook it used for the  deep nested routes.Given hook reqires the chertain id  when the thoader funciton is used.
+
 const EditEventPage = () => {
-  return <h1>Edit page here</h1>;
+  const data = useRouteLoaderData("event-detail");
+  return <EventForm event={data.event} method="patch" />;
 };
 
 export default EditEventPage;
